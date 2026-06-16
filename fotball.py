6ef16bd772866c9786b34e6d07fa6215a3eebf2f
Thevini2041 <163761191+Thevini2041@users.noreply.github.com>
2026-06-16 17:04:49 -0600

    fotball.py

--- fotball.py
+++ fotball.py
@@ -270,3 +270,8 @@ scroll.pack(side="right", fill="y")
 partidos_actuales = []
 
 root.mainloop()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
